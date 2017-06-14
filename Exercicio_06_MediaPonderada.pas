2c199b5d43c1eed67fe841e4fb74e16e9cae3f0d
Ader Santos <adersantos@yahoo.com.br>
2017-06-14 16:16:11 -0300

    Add files via upload

--- Exercicio_06_MediaPonderada.pas
+++ Exercicio_06_MediaPonderada.pas
@@ -1,10 +1,11 @@
 program MediaPonderada;
 
-var n1, n2, n3, i: integer;
-var r: real;
-
-Begin
+var n1, n2, n3, i : integer;
+    r, soma: real;
+	M: array [1..50] of real;
 	
+Begin
+	soma := 0;
 	for i := 1 to 50 do
 	begin
 		
@@ -12,9 +13,16 @@ Begin
 		n2 := i + 1;
 		n3 := i + 2;
 		
-		r := (n1*1+ n2*2 + n3*3) / (1+2+3);
-		writeln('Media ponderada do ',i,'º aluno: ', r);
-		
+		M[i] := (n1*1+ n2*2 + n3*3) / (1+2+3);
+		r := M[i];
+		soma := soma + r;
+		if (r > 7) then
+			writeln('Aluno ', i, ': Aprovado')
+		else
+			writeln('Aluno ', i, ': Reprovado');
+			
+		writeln('Media ponderada do ',i,'º aluno: ', r);		
 	end;
-
+	
+	writeln('Media geral da turma: ', soma / 50);	
 End.
